11e6a6a7650d0b03ae8125ecbb68bf421ed7d107
Gnong <jinhongdev@gmail.com>
2021-03-19 14:28:44 +0900

    API Verbs part Two

--- gcha/src/api.js
+++ gcha/src/api.js
@@ -8,4 +8,14 @@ const api = axios.create({
     }
 });
 
-export default api;
+export const moviesApi = {
+    nowPlaying: () => api.get("movie/nowplaying"),
+    upcoming: () => api.get("moive/upcoming"),
+    popular: () => api.get("movie/popular"),
+};
+
+export const tvApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/ariring_today")
+}
