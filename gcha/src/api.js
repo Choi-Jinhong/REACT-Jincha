f16a9ddd89600f63321b180f99d246043e6f0c98
Gnong <jinhongdev@gmail.com>
2021-03-19 14:39:36 +0900

    API Verbs part Two

--- gcha/src/api.js
+++ gcha/src/api.js
@@ -12,10 +12,30 @@ export const moviesApi = {
     nowPlaying: () => api.get("movie/nowplaying"),
     upcoming: () => api.get("moive/upcoming"),
     popular: () => api.get("movie/popular"),
+    movieDetail: id => api.get(`moive/${id}`, {
+        params: {
+            append_to_response:"videos"
+        }
+    }),
+    search: term => api.get(`search/movie`, {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
 };
 
 export const tvApi = {
     topRated: () => api.get("tv/top_rated"),
     popular: () => api.get("tv/popular"),
-    airingToday: () => api.get("tv/ariring_today")
+    airingToday: () => api.get("tv/ariring_today"),
+    showDetail: id => api.get(`tv/${id}`, {
+        params: {
+            append_to_response:"videos"
+        }
+    }),
+    search: term => api.get(`search/tv`, {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
 }
