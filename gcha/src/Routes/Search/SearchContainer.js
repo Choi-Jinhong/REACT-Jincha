6bee9ff8baf357891236b22a3e44142f462a208d
Gnong <jinhongdev@gmail.com>
2021-03-21 21:11:48 +0900

    SearchPresenter Component

--- gcha/src/Routes/Search/SearchContainer.js
+++ gcha/src/Routes/Search/SearchContainer.js
@@ -5,26 +5,36 @@ import SearchPresenter from "./SearchPresenter";
 export default class extends React.Component {
     state = {
         movieResults: null,
+        showResults: null,
         popular: null,
         searchTerm: "",
         error: null,
         loading: false
     }
 
-    handleSubmit = () => {
+    handleSubmit = (event) => {
+        event.preventDefault();
         const { searchTerm } = this.state;
         if (searchTerm !== "") {
             this.searchByTerm(searchTerm);
         }
     }
 
+    updateTerm = (event) => {
+        const { target: { value } } = event;
+        this.setState({
+            searchTerm: value
+        })
+    }
+
     searchByTerm = async () => {
         const { searchTerm } = this.state;
         try {
             this.setState({ loading: true });
-            const { data: { movieResults } } = await moviesApi.search(searchTerm);
-            const { data: { showResults } } = await tvApi.search(searchTerm);
-            this.setState({ movieResults, showResults});
+            const { data: { results: movieResults } } = await moviesApi.search(searchTerm);
+            const { data: { results: showResults } } = await tvApi.search(searchTerm);
+            console.log(showResults);
+            this.setState({ movieResults, showResults });
         } catch {
             this.setState({ error: "Can't find results." });
         } finally {
@@ -42,6 +52,7 @@ export default class extends React.Component {
                 error={error}
                 loading={loading}
                 handleSubmit={this.handleSubmit}
+                updateTerm={this.updateTerm}
             />
         );
     }
