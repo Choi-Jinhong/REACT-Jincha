7386c4ea1a0f44791b2cbae90db14dd2c1dea06f
Gnong <jinhongdev@gmail.com>
2021-03-21 20:19:05 +0900

    TVPresenter and Loader Components

--- gcha/src/Routes/Home/HomePresenter.js
+++ gcha/src/Routes/Home/HomePresenter.js
@@ -2,19 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
+import Loader from "Components/Loader";
 
 const Container = styled.div`
     padding: 0 10px;
 `;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-    loading ? null : (
-        <Container>
-            {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => movie.title)}</Section>}
-            {upcoming && upcoming.length > 0 && <Section title="Upcoming Moives">{upcoming.map(movie => movie.title)}</Section>}
-            {popular && popular.length > 0 && <Section title="Popular Moives">{popular.map(movie => movie.title)}</Section>}
-        </Container>
-    );
+    loading ? (
+        <Loader />
+    ) : (
+            <Container>
+                {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
+                {upcoming && upcoming.length > 0 && <Section title="Upcoming Moives">{upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
+                {popular && popular.length > 0 && <Section title="Popular Moives">{popular.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
+            </Container>
+        );
 
 HomePresenter.propTypes = {
     nowPlaying: PropTypes.array,
