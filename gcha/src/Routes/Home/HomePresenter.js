27482231dab6986284c277adcb9572059c361555
Gnong <jinhongdev@gmail.com>
2021-03-22 12:42:50 +0900

    Poster Component part One

--- gcha/src/Routes/Home/HomePresenter.js
+++ gcha/src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
     padding: 0 10px;
@@ -14,9 +15,9 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
         <Loader />
     ) : (
             <Container>
-                {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
-                {upcoming && upcoming.length > 0 && <Section title="Upcoming Moives">{upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
-                {popular && popular.length > 0 && <Section title="Popular Moives">{popular.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
+                {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => (<Poster />))}</Section>}
+                {upcoming && upcoming.length > 0 && <Section title="Upcoming Moives">{upcoming.map(movie => (<Poster />))}</Section>}
+                {popular && popular.length > 0 && <Section title="Popular Moives">{popular.map(movie => (<Poster />))}</Section>}
                 {error && <Message text={error} color="#e74c3c" />}
             </Container>
         );
